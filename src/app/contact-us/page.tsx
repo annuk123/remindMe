2b07d863bfad6738967718c11216350890fdba13
Annu Kumari <anuk35168@gmail.com>
2025-10-21 15:12:41 +0530

    remove unused lines

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -106,9 +106,6 @@ const handleSubmit = async (e: React.FormEvent) => {
               </a>
             </div>
 
-            <div className="flex items-center gap-3">
-              <MapPin className="text-blue-600 w-5 h-5" />
-            </div>
           </div>
 
           {/* <p className="text-slate-500 text-sm mt-8">
